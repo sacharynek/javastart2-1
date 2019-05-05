d56dacebbf3763a4f75d1f75ad9771888c13c478
tomaszlach86 <tomaszlach86@gmail.com>
2019-05-05 13:58:37 +0200

    final solution

--- src/main/java/MovieLibrary.java
+++ src/main/java/MovieLibrary.java
@@ -1,4 +1,5 @@
 class MovieLibrary {
+
     public static void main(String[] args) {
         Movie movie = new Movie();
         movie.title = "Forrest Gump";
@@ -6,12 +7,13 @@ class MovieLibrary {
                 "z niedowładem kończyn, który staje się miliarderem i bohaterem wojny w Wietnamie.";
         movie.director = "Robert Zemeckis";
         movie.rating = 9;
+        movie.oscars = true;
 
         System.out.println("Informacje o filmie:");
         System.out.printf("Tytuł: %s\n", movie.title);
         System.out.printf("Opis: %s\n", movie.description);
         System.out.printf("Reżyser: %s\n", movie.director);
-        System.out.printf("Ocena: %f/10\n", movie.rating);
+        System.out.printf("Ocena: %.0f/10\n", movie.rating);
         System.out.printf("Czy otrzymał Oskara: %s\n", movie.oscars);
     }
 }
